05200c2bfae87a27f06b3d6ef3a2a8b3e28d4d14
Itallo Aleandro <italloaleandro@gmail.com>
2025-01-15 15:34:01 -0300

    ok

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,7 @@ import { User_routes } from "./presentation/user_routes";
 import { Quiz_routes } from "./presentation/quiz_routes";
 import { Resultado_routes } from "./presentation/resultado_routes";
 import { Historico_routes } from "./presentation/historico_routes";
+import { Imagem_routes } from "./presentation/imagem_routes";
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ new User_routes(app);
 new Quiz_routes(app);
 new Resultado_routes(app);
 new Historico_routes(app);
+new Imagem_routes(app);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
