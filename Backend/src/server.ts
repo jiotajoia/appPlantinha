086a439a58d0cfd5e98473d521c0879ce093f35b
Jio <bleibeducuz@gmail.com>
2025-02-03 18:00:19 -0300

    type module

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,25 +1,29 @@
 import express from 'express';
-import { UserRoutes } from "./presentation/routes/user_routes";
-import { QuizRoutes } from "./presentation/routes/pergunta_routes";
-import { ResultadoRoutes } from "./presentation/routes/resultado_routes";
-import { ImagemRoutes } from "./presentation/routes/imagem_routes";
-import { HistoricoRoutes } from "./presentation/routes/historico_routes";
+// import { UserRoutes } from "./presentation/routes/user_routes";
+// import { QuizRoutes } from "./presentation/routes/pergunta_routes";
+// import { ResultadoRoutes } from "./presentation/routes/resultado_routes";
+// import { ImagemRoutes } from "./presentation/routes/imagem_routes";
+// import { HistoricoRoutes } from "./presentation/routes/historico_routes";
 import dotenv from 'dotenv';
-import { Password_routes } from './presentation/routes/password_routes';
+//import { Password_routes } from './presentation/routes/password_routes';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
+console.log('oi')
+
+console.log(process.env);
+
 app.use(express.json());
 
-new UserRoutes(app);
-new QuizRoutes(app);
-new ResultadoRoutes(app);
-new HistoricoRoutes(app);
-new ImagemRoutes(app);
-new Password_routes(app);
+// new UserRoutes(app);
+// new QuizRoutes(app);
+// new ResultadoRoutes(app);
+// new HistoricoRoutes(app);
+// new ImagemRoutes(app);
+// new Password_routes(app);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
